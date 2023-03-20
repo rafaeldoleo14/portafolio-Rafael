eed38b308862a537f4fa379743b533fbdc98183b
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-03-19 20:05:04 -0400

    feat: added new project

--- src/portafolio/data/projects.js
+++ src/portafolio/data/projects.js
@@ -1,5 +1,6 @@
 
 import criptoProject from '../../assets/img/projectsImg/criptoProject.png';
+import fullSecurityProject from '../../assets/img/projectsImg/full security.jpg';
 
 export const projects = [
 
@@ -9,6 +10,14 @@ export const projects = [
         img: criptoProject,
         skillUsed: ['React', 'Typescript', 'CSS',],
         link: 'https://consultar-criptop-rafael.netlify.app/'
+    },
+
+    {
+        id: 2,
+        title: 'Full Security',
+        img: fullSecurityProject,
+        skillUsed: ['React', 'CSS',],
+        link: 'https://full-security.netlify.app/'
     },
 
 
