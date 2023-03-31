8daef01abfa779638145418b711a5fe2f79310e3
Rafael Francisco <rafaeldeoleo14@gmail.com>
2023-03-31 18:32:43 -0400

    fix: little changes in navbar

--- src/portafolio/hooks/useClick.js
+++ src/portafolio/hooks/useClick.js
@@ -4,16 +4,11 @@ import React, { useEffect, useState } from 'react'
 export const useClick = () => {
  
     const [btnState, setBtnState] = useState(false);
-    const [isToggled, setIsToggled] = useState(false);
 
     const handleClick = ()=>{
         setBtnState(btnState => !btnState);
         setIsToggled(prevState => !prevState);
     }
-
-    useEffect(()=>{
-        document.body.style.overflowY = isToggled ? 'hidden' : 'auto'
-    },[isToggled])
 
     return{
 
