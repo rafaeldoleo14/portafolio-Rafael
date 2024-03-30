e057eed0d49b6d5275083ae3cf50c71f41a19533
Rafael Francisco <rafaeldeoleo14@gmail.com>
2024-03-29 20:14:30 -0400

    feat: added new skills and cv

--- src/portafolio/hooks/useDownloadPdf.js
+++ src/portafolio/hooks/useDownloadPdf.js
@@ -1,6 +1,6 @@
 
 import React from 'react'
-import pdfURL from '../../assets/cv/RafaelDoleo-CV.pdf'
+import pdfURL from '../../assets/cv/RafaelFranciscoCV.pdf'
 
 export const useDownloadPdf = () => {
 
@@ -10,7 +10,7 @@ export const useDownloadPdf = () => {
         const link = document.createElement('a');
         link.href = pdfURL;
         // Establece el atributo 'download' para indicar que se debe descargar el archivo en lugar de abrirlo
-        link.download = 'RafaelDoleoCV.pdf';
+        link.download = 'RafaelFranciscoCV.pdf';
         // Dispara el evento click en el elemento <a>
         link.click();
     }
